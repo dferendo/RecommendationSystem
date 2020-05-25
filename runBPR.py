b2c15205ec73426626f021e61743334c92aafc16
Dylan <dylanfrendo@gmail.com>
2020-05-26 00:33:38 +0200

    Added negative sampling

--- runBPR.py
+++ runBPR.py
@@ -1,19 +1,70 @@
 from utils.arg_parser import extract_args_from_json
-from utils.data_provider import split_dataset, get_sparse_df
+from utils.data_provider import split_dataset
 from utils.reset_seed import set_seeds
 from models import Random
 
 import pandas as pd
 import numpy as np
 
+from torch.utils.data import Dataset
+
+
+class BPRData(Dataset):
+    def __init__(self, training_examples, train_matrix, neg_sample_per_training_example, is_training):
+        self.training_examples = training_examples
+        self.train_matrix = train_matrix
+        self.neg_sample_per_training_example = neg_sample_per_training_example
+        self.is_training = is_training
+        self.num_of_movies = len(train_matrix.columns)
+
+        self.features_fill = []
+
+    def negative_sampling(self):
+        # Sampling is only needed when training
+        assert self.is_training
+
+        for index, interaction in self.training_examples.iterrows():
+            user, movie = interaction['userId'], interaction['movieId']
+            user_movies = self.train_matrix.loc[user]
+
+            for idx in range(self.neg_sample_per_training_example):
+                negative_sampled_movie = np.random.randint(self.num_of_movies)
+
+                # In this case, we need to use iloc since the random number generated that not correspond to a movieId
+                # but rather to an array index
+                while user_movies.iloc[negative_sampled_movie] != 0:
+                    negative_sampled_movie = np.random.randint(self.num_of_movies)
+
+                self.features_fill.append([user, movie, user_movies.index[negative_sampled_movie]])
+
+    def __len__(self):
+        if self.is_training:
+            return self.neg_sample_per_training_example * len(self.training_examples)
+
+        return len(self.training_examples)
+
+    def __getitem__(self, idx):
+        # If in training, we need to use the negatively sampled item with the interacted item
+        if self.is_training:
+            features = self.features_fill
+        else:
+            features = self.training_examples
+
+        # TODO:
+        user = features[idx][0]
+        item_i = features[idx][1]
+        item_j = features[idx][2] if \
+            self.is_training else features[idx][1]
+        return user, item_i, item_j
+
 
 def experiments_run():
     configs = extract_args_from_json()
     set_seeds(configs['seed'])
 
-    df_train, df_val, df_test = split_dataset(configs)
-    get_sparse_df(df_train)
+    df_train, df_val, df_test, df_train_sparse = split_dataset(configs)
 
+    BPRData(df_train, df_train_sparse, 5, True).negative_sampling()
 
     # R_df = df_train.pivot(index='userId', columns='movieId', values='rating').fillna(0)
 
--- utils/data_provider.py
+++ utils/data_provider.py
@@ -5,9 +5,31 @@ from pandas.api.types import CategoricalDtype
 import torch.utils.data as data
 
 
+def get_sparse_df(df):
+    """
+    Note: This process is done to conserve memory. If memory is not an issue, simply use pivot
+    (Ie df.pivot(index='userId', columns='movieId', values='rating').fillna(0))
+    :param df:
+    :return:
+    """
+    users_category = CategoricalDtype(sorted(df['userId'].unique()), ordered=True)
+    movies_category = CategoricalDtype(sorted(df['movieId'].unique()), ordered=True)
+
+    row = df['userId'].astype(users_category).cat.codes
+    col = df['movieId'].astype(movies_category).cat.codes
+
+    sparse_matrix = csr_matrix((df["rating"], (row, col)),
+                               shape=(users_category.categories.size, movies_category.categories.size))
+
+    sparse_df = pd.DataFrame.sparse.from_spmatrix(sparse_matrix, index=users_category.categories,
+                                                  columns=movies_category.categories)
+
+    return sparse_df
+
+
 def split_dataset(configs):
     # Loading
-    df_all = pd.read_csv(configs['data_location'])
+    df_all = pd.read_csv(configs['data_location'], dtype={'userId': np.int32, 'movieId': np.int32})
 
     # Make dataset implicit (ie User had interaction/User did not have interaction)
     df_all = df_all[df_all['rating'] >= configs['implicit_rating']]
@@ -36,66 +58,4 @@ def split_dataset(configs):
     # Remove any movies that do not appear in the training set from the validation set
     df_val = df_val.loc[df_val['movieId'].isin(df_train['movieId'].unique())]
 
-    return df_train, df_val, df_test
-
-
-def get_sparse_df(df):
-    """
-    Note: This process is done to conserve memory. If memory is not an issue, simply use pivot
-    (Ie df.pivot(index='userId', columns='movieId', values='rating').fillna(0))
-    :param df:
-    :return:
-    """
-    users_category = CategoricalDtype(sorted(df['userId'].unique()), ordered=True)
-    movies_category = CategoricalDtype(sorted(df['movieId'].unique()), ordered=True)
-
-    row = df['userId'].astype(users_category).cat.codes
-    col = df['movieId'].astype(movies_category).cat.codes
-
-    sparse_matrix = csr_matrix((df["rating"], (row, col)),
-                               shape=(users_category.categories.size, movies_category.categories.size))
-
-    sparse_df = pd.DataFrame.sparse.from_spmatrix(sparse_matrix, index=users_category.categories,
-                                                  columns=movies_category.categories)
-
-    return sparse_df
-
-
-# class BPRData(data.dataset):
-#     def __init__(self, examples, num_item, train_mat, neg_sample_per_training_example, is_training):
-#         self.neg_sample_per_training_example = neg_sample_per_training_example
-#         self.is_training = is_training
-#
-#     def negative_sampling(self):
-#         # Sampling is only needed when training
-#         assert self.is_training
-#
-#         self.features_fill = []
-#
-#         for x in self.features:
-#             u, i = x[0], x[1]
-#             for t in range(self.num_ng):
-#                 j = np.random.randint(self.num_item)
-#
-#                 while (u, j) in self.train_mat:
-#                     j = np.random.randint(self.num_item)
-#
-#                 self.features_fill.append([u, i, j])
-#
-#     def __len__(self):
-#         if self.is_training:
-#             return self.num_ng * len(self.features)
-#
-#         return len(self.features)
-#
-#     def __getitem__(self, idx):
-#         if self.is_training:
-#             features = self.features_fill
-#         else:
-#             features = self.features
-#
-#         user = features[idx][0]
-#         item_i = features[idx][1]
-#         item_j = features[idx][2] if \
-#             self.is_training else features[idx][1]
-#         return user, item_i, item_j
+    return df_train, df_val, df_test, get_sparse_df(df_train)
