eb87830afbb0106c56f915d6f4bc267735fe6994
Dylan <dylanfrendo@gmail.com>
2020-05-26 15:45:16 +0200

    Returning indexes instead of ids from negative sampling

---
+++ dataloaders/NegativeSamplingDataLoader.py
@@ -0,0 +1,79 @@
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class NSData(Dataset):
+    def __init__(self, training_examples, train_matrix, neg_sample_per_training_example, is_training):
+        self.training_examples = training_examples
+        self.train_matrix = train_matrix
+        self.neg_sample_per_training_example = neg_sample_per_training_example
+        self.is_training = is_training
+        self.num_of_movies = len(train_matrix.columns)
+        self.all_movies_that_can_be_sampled = np.array(train_matrix.columns)
+
+        self.interactions_with_negative_sampling = None
+
+    def negative_sampling(self):
+        # Sampling is only needed when training
+        assert self.is_training
+        assert self.neg_sample_per_training_example > 0
+
+        grouped_users = self.training_examples.groupby(['userId'])['movieId'].apply(list)
+
+        all_negative_samples = []
+
+        for user_id, user_interactions in grouped_users.items():
+            # Get the possible index of movieIds that we can sample for this user
+            movies_to_sample = np.setxor1d(self.all_movies_that_can_be_sampled, user_interactions)
+
+            # Generate all the negative samples (Not sure about the efficiency of np.choice)
+            negative_samples_for_user = np.random.choice(movies_to_sample,
+                                                         size=self.neg_sample_per_training_example * len(user_interactions))
+
+            # Reshape so that for every movie, we have x negative samples
+            negative_samples_for_user = np.reshape(negative_samples_for_user,
+                                                   (len(user_interactions), self.neg_sample_per_training_example))
+
+            users_interactions_matrix = np.expand_dims(np.array(user_interactions), axis=1)
+            user_id_matrix = np.full((len(user_interactions), 1), user_id)
+
+            # Concat with the userId, the movieId (positive interaction)
+            user_positive_interactions = np.hstack((user_id_matrix, users_interactions_matrix))
+
+            # For every negative sample column, concat it with the user true interaction
+            for idx in range(self.neg_sample_per_training_example):
+                column = negative_samples_for_user[:, idx]
+                column_matrix = np.expand_dims(np.array(column), axis=1)
+
+                # Concat with the user interactions
+                negative_samples = np.hstack((user_positive_interactions, column_matrix))
+
+                all_negative_samples.append(negative_samples)
+
+        self.interactions_with_negative_sampling = np.vstack(all_negative_samples)
+
+    def __len__(self):
+        if self.is_training:
+            return self.neg_sample_per_training_example * len(self.training_examples)
+
+        return len(self.training_examples)
+
+    def __getitem__(self, idx):
+        # If in training, we need to use the negatively sampled item with the interacted item
+        if self.is_training:
+            user = self.interactions_with_negative_sampling[idx, 0]
+            item_i = self.interactions_with_negative_sampling[idx, 1]
+            item_j = self.interactions_with_negative_sampling[idx, 2]
+        else:
+            user = self.training_examples.iloc[idx]['userId']
+            item_i = self.training_examples.iloc[idx]['movieId']
+            item_j = None
+
+        # Convert from ids to indexes for the embedding
+        user = self.train_matrix.index.get_loc(user)
+        item_i = self.train_matrix.columns.get_loc(item_i)
+
+        if item_j is not None:
+            item_j = self.train_matrix.columns.get_loc(item_j)
+
+        return user, item_i, item_j
--- runBPR.py
+++ runBPR.py
@@ -2,6 +2,7 @@ from utils.arg_parser import extract_args_from_json
 from utils.data_provider import split_dataset
 from utils.reset_seed import set_seeds
 from models import BayesianPR
+from dataloaders.NegativeSamplingDataLoader import NSData
 
 import numpy as np
 
@@ -11,85 +12,13 @@ import torch.nn as nn
 from torch.optim.adam import Adam
 
 
-class BPRData(Dataset):
-    def __init__(self, training_examples, train_matrix, neg_sample_per_training_example, is_training):
-        self.training_examples = training_examples
-        self.train_matrix = train_matrix
-        self.neg_sample_per_training_example = neg_sample_per_training_example
-        self.is_training = is_training
-        self.num_of_movies = len(train_matrix.columns)
-        self.all_movies_that_can_be_sampled = np.array(train_matrix.columns)
-
-        self.interactions_with_negative_sampling = None
-
-    def negative_sampling(self):
-        # Sampling is only needed when training
-        assert self.is_training
-        assert self.neg_sample_per_training_example > 0
-
-        grouped_users = self.training_examples.groupby(['userId'])['movieId'].apply(list)
-
-        all_negative_samples = []
-
-        for user_id, user_interactions in grouped_users.items():
-            # Get the possible index of movieIds that we can sample for this user
-            movies_to_sample = np.setxor1d(self.all_movies_that_can_be_sampled, user_interactions)
-
-            # Generate all the negative samples (Not sure about the efficiency of np.choice)
-            negative_samples_for_user = np.random.choice(movies_to_sample,
-                                                         size=self.neg_sample_per_training_example * len(user_interactions))
-
-            # Reshape so that for every movie, we have x negative samples
-            negative_samples_for_user = np.reshape(negative_samples_for_user,
-                                                   (len(user_interactions), self.neg_sample_per_training_example))
-
-            users_interactions_matrix = np.expand_dims(np.array(user_interactions), axis=1)
-            user_id_matrix = np.full((len(user_interactions), 1), user_id)
-
-            # Concat with the userId, the movieId (positive interaction)
-            user_positive_interactions = np.hstack((user_id_matrix, users_interactions_matrix))
-
-            # For every negative sample column, concat it with the user true interaction
-            for idx in range(self.neg_sample_per_training_example):
-                column = negative_samples_for_user[:, idx]
-                column_matrix = np.expand_dims(np.array(column), axis=1)
-
-                # Concat with the user interactions
-                negative_samples = np.hstack((user_positive_interactions, column_matrix))
-
-                all_negative_samples.append(negative_samples)
-
-        self.interactions_with_negative_sampling = np.vstack(all_negative_samples)
-
-    def __len__(self):
-        if self.is_training:
-            return self.neg_sample_per_training_example * len(self.training_examples)
-
-        return len(self.training_examples)
-
-    def __getitem__(self, idx):
-        # If in training, we need to use the negatively sampled item with the interacted item
-        if self.is_training:
-            features = self.features_fill
-        else:
-            features = self.training_examples
-
-        # TODO:
-        user = features[idx][0]
-        item_i = features[idx][1]
-        item_j = features[idx][2] if \
-            self.is_training else features[idx][1]
-
-        return user, item_i, item_j
-
-
 def experiments_run():
     configs = extract_args_from_json()
     set_seeds(configs['seed'])
 
     df_train, df_val, df_test, df_train_matrix = split_dataset(configs)
 
-    train_dataset = BPRData(df_train, df_train_matrix, configs['negative_samples'], True)
+    train_dataset = NSData(df_train, df_train_matrix, configs['negative_samples'], True)
     train_loader = DataLoader(train_dataset, batch_size=configs['batch_size'], shuffle=True, num_workers=4)
 
     model = BayesianPR.BPR(len(df_train_matrix.index), len(df_train_matrix.columns), configs['hidden_dims'])
