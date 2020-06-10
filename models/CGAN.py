b11ac77aca1fc8e404ef42427d900a1a0f04d08e
Dylan <dylanfrendo@gmail.com>
2020-06-10 15:15:52 +0200

    Added GAN

---
+++ models/CGAN.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, num_of_movies, slate_size, embed_dims, noise_dim, hidden_layers_dims):
+        super(Generator, self).__init__()
+        self.num_of_movies = num_of_movies
+        self.embed_dims = embed_dims
+        self.noise_dim = noise_dim
+        self.slate_size = slate_size
+
+        # Index work from 0 - (num_of_movies - 1). Thus, we use num_of_movies as a padding index
+        self.padding_idx = self.num_of_movies
+
+        # +1 for the padding index
+        self.embedding_movies = torch.nn.Embedding(num_embeddings=self.num_of_movies + 1,
+                                                   embedding_dim=self.embed_dims,
+                                                   padding_idx=self.padding_idx)
+
+        layers_block = []
+        in_dims = self.embed_dims + self.noise_dim
+
+        for out_dims in hidden_layers_dims:
+            layers_block.extend(self.gen_block(in_dims, out_dims))
+            in_dims = out_dims
+
+        self.model_layers = nn.Sequential(
+            *layers_block
+        )
+
+        # Will be used to hold the output linear layers
+        self.output_dict = nn.ModuleDict()
+        self.output_dict['output_tanh'] = nn.Tanh()
+        self.output_dict['softmax'] = nn.Softmax(dim=1)
+
+        for i in range(self.slate_size):
+            self.output_dict[f'output_linear{i}'] = nn.Linear(in_dims, self.num_of_movies)
+
+    def forward(self, user_interactions_with_padding, number_of_interactions_per_user, noise):
+        movie_embedding = self.embedding_movies(user_interactions_with_padding)
+        user_embedding = torch.sum(movie_embedding, dim=1) / number_of_interactions_per_user
+
+        gen_input = torch.cat((noise, user_embedding), dim=-1)
+        out = self.model_layers(gen_input)
+
+        slate_outputs = []
+
+        for i in range(self.slate_size):
+            slate_embed_output = self.output_dict[f'output_linear{i}'](out)
+            slate_embed_output = self.output_dict['output_tanh'](slate_embed_output)
+
+            slate_embed_output = self.output_dict['softmax'](slate_embed_output)
+
+            # TODO: Inference
+            # movie_indexes = torch.argmax(slate_embed_output, dim=1).unsqueeze(dim=0).T
+
+            slate_outputs.append(slate_embed_output)
+
+        slates = torch.cat(slate_outputs, dim=1)
+
+        return slates
+
+    @staticmethod
+    def gen_block(in_feat, out_feat, normalize=True, dropout=0.2):
+        layers = [nn.Linear(in_feat, out_feat)]
+
+        if normalize:
+            layers.append(nn.BatchNorm1d(num_features=out_feat))
+        if dropout:
+            layers.append(nn.Dropout(dropout))
+
+        layers.append(nn.LeakyReLU(0.01, inplace=True))
+
+        return layers
+
+
+class Discriminator(nn.Module):
+    def __init__(self, num_of_movies, slate_size, embed_dims, hidden_layers_dims):
+        super(Discriminator, self).__init__()
+        self.num_of_movies = num_of_movies
+        self.embed_dims = embed_dims
+
+        # Index work from 0 - (num_of_movies - 1). Thus, we use num_of_movies as a padding index
+        self.padding_idx = self.num_of_movies
+
+        # +1 for the padding index
+        self.embedding_movies = torch.nn.Embedding(num_embeddings=self.num_of_movies + 1,
+                                                   embedding_dim=self.embed_dims,
+                                                   padding_idx=self.padding_idx)
+
+        layers_block = []
+
+        input_dims = (self.num_of_movies * slate_size) + self.embed_dims
+
+        for out_dims in hidden_layers_dims:
+            layers_block.extend(self.dis_block(input_dims, out_dims))
+            input_dims = out_dims
+
+        self.model_layers = nn.Sequential(
+            *layers_block,
+            nn.Linear(input_dims, 1)
+        )
+
+    def forward(self, slate_input, user_interactions_with_padding, number_of_interactions_per_user):
+        # Concatenate label embedding and image to produce input
+        movie_embedding = self.embedding_movies(user_interactions_with_padding)
+        user_embedding = torch.sum(movie_embedding, dim=1) / number_of_interactions_per_user
+
+        dim_input = torch.cat((slate_input, user_embedding), dim=1)
+
+        out = self.model_layers(dim_input)
+
+        return out
+
+    @staticmethod
+    def dis_block(in_feat, out_feat, normalize=False, dropout=0.2):
+        layers = [nn.Linear(in_feat, out_feat)]
+
+        if normalize:
+            layers.append(nn.BatchNorm1d(num_features=out_feat))
+        if dropout:
+            layers.append(nn.Dropout(dropout))
+
+        layers.append(nn.LeakyReLU(0.01, inplace=True))
+
+        return layers
--- runCGAN.py
+++ runCGAN.py
@@ -6,17 +6,30 @@ from dataloaders.SlateFormation import SlateFormationDataLoader
 from torch.utils.data import DataLoader
 import os
 import pandas as pd
+from utils.experiment_builder import ExperimentBuilder
 
 
-def experiments_run():
-    configs = extract_args_from_json()
-    set_seeds(configs['seed'])
+class GreedyMLPExperimentBuilder(ExperimentBuilder):
 
-    df_train, df_test, df_train_matrix, df_test_matrix = split_dataset(configs)
-    movies_categories = load_movie_categories(configs)
+    def pre_epoch_init_function(self):
+        pass
+
+    def loss_function(self, values):
+        pass
+
+    def forward_model_training(self, values_to_unpack):
+        pass
+
+    def forward_model_test(self, values_to_unpack):
+        pass
+
+
+def get_data_loaders(configs):
+    df_train, df_test, df_train_matrix, df_test_matrix, movies_categories = split_dataset(configs)
 
     slate_formation_file_name = 'sf_{}_{}_{}.csv'.format(configs['slate_size'],
-                                                         '-'.join(str(e) for e in configs['negative_sampling_for_slates']),
+                                                         '-'.join(
+                                                             str(e) for e in configs['negative_sampling_for_slates']),
                                                          configs['is_training'])
     slate_formation_file_location = os.path.join(configs['save_location'], slate_formation_file_name)
 
@@ -29,9 +42,88 @@ def experiments_run():
                                                    slate_formation_file_location)
 
     train_dataset = SlateFormationDataLoader(slate_formation, df_train_matrix)
-    train_loader = DataLoader(train_dataset, batch_size=configs['batch_size'], shuffle=True, num_workers=4,
+    train_loader = DataLoader(train_dataset, batch_size=configs['train_batch_size'], shuffle=True, num_workers=4,
                               drop_last=True)
+
+    return train_loader
+
+
+def experiments_run():
+    configs = extract_args_from_json()
+    set_seeds(configs['seed'])
+
+    train_loader = get_data_loaders(configs)
+
+    for values in train_loader:
+        print(values)
+
+
 
 
 if __name__ == '__main__':
     experiments_run()
+    # batch_size = 2
+    # num_of_movies = 5
+    # slate_size = 6
+    # embed_dims = 10
+    # noise_hidden_dims = 20
+    # hidden_layers_dims_gen = [20, 40, 60]
+    # hidden_layers_dims_dis = [512, 512, 512]
+    #
+    # gen = Generator(num_of_movies, slate_size, embed_dims, noise_hidden_dims, hidden_layers_dims_gen)
+    #
+    # slates = torch.LongTensor([[0, 1, 2, 3, 4, 4], [0, 0, 0, 1, 1, 1]]).cuda()
+    # user_interactions_with_padding = torch.LongTensor([[2, 3, 4], [0, 1, 5]]).cuda()
+    # number_of_interactions_per_user = torch.LongTensor([[3], [2]]).cuda()
+    #
+    # for epoch in range(5):
+    #     dis = Discriminator(num_of_movies, slate_size, embed_dims, hidden_layers_dims_dis)
+    #
+    #     # Loss functions
+    #     criterion = torch.nn.BCELoss()
+    #
+    #     if torch.cuda.is_available():
+    #         gen.cuda()
+    #         dis.cuda()
+    #         criterion.cuda()
+    #
+    #     optimizer_G = torch.optim.Adam(gen.parameters(), lr=0.0002, betas=(0.5, 0.5))
+    #     optimizer_D = torch.optim.Adam(dis.parameters(), lr=0.0002, betas=(0.5, 0.5))
+    #
+    #     # Adversarial ground truths
+    #     valid = torch.tensor(np.full((batch_size, 1), 1.0), dtype=torch.float32, requires_grad=False).cuda()
+    #     fake = torch.tensor(np.full((batch_size, 1), 0.0), dtype=torch.float32, requires_grad=False).cuda()
+    #
+    #     optimizer_G.zero_grad()
+    #
+    #     noise = torch.normal(0, 1, size=(batch_size, noise_hidden_dims)).cuda()
+    #
+    #     gen_fake = gen(user_interactions_with_padding, number_of_interactions_per_user, noise)
+    #     validity = dis(gen_fake, user_interactions_with_padding, number_of_interactions_per_user)
+    #
+    #     g_loss = criterion(validity, fake)
+    #     g_loss.backward()
+    #
+    #     plot_grad_flow(gen.named_parameters())
+    #
+    #     optimizer_G.step()
+    #
+    #     '''
+    #     Training Discriminator
+    #     '''
+    #     optimizer_D.zero_grad()
+    #
+    #     # Loss for real images
+    #     validity_real = dis(slates, user_interactions_with_padding, number_of_interactions_per_user)
+    #     d_real_loss = criterion(validity_real, valid)
+    #
+    #     # Loss for fake images
+    #     validity_fake = dis(gen_fake.detach(), user_interactions_with_padding, number_of_interactions_per_user)
+    #     d_fake_loss = criterion(validity_fake, fake)
+    #
+    #     # Total discriminator loss
+    #     d_loss = (d_real_loss + d_fake_loss) / 2
+    #
+    #     d_loss.backward()
+    #     optimizer_D.step()
+
