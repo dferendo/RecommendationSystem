d7c3f561f1e6cd8382d1a12880104cd882fbef9f
Dylan <dylanfrendo@gmail.com>
2020-06-14 23:12:26 +0200

    Fixed issue with popK

--- models/PopK.py
+++ models/PopK.py
@@ -6,17 +6,24 @@ class PopularKSlateGeneration:
     """
     Generating slates by uniform distribution
     """
-    def __init__(self, slate_size, training_interactions, batch_size):
+    def __init__(self, slate_size, training_interactions, user_movie_matrix, batch_size):
         self.slate_size = slate_size
         self.batch_size = batch_size
+        self.user_movie_matrix = user_movie_matrix
 
-        popK = training_interactions.groupby('movieId')['userId']\
+        most_popular_movie_ids = training_interactions.groupby('movieId')['userId']\
             .count()\
             .sort_values(ascending=False)\
             .head(self.slate_size)\
             .index.tolist()
 
-        self.batched_popK = torch.from_numpy(np.tile(popK, (self.batch_size, 1)))
+        most_popular_movie_index = list(map(lambda movie_id: self.user_movie_matrix.columns.get_loc(movie_id),
+                                            most_popular_movie_ids))
+
+        self.batched_popK = torch.from_numpy(np.tile(most_popular_movie_index, (self.batch_size, 1)))
 
     def forward(self):
         return self.batched_popK
+
+    def reset_parameters(self):
+        pass
