372f30affd02f5cc7f32be7b3e99e2ede401b9df
Dylan <dylanfrendo@gmail.com>
2020-06-17 22:10:40 +0200

    WIP

--- runCGAN.py
+++ runCGAN.py
@@ -10,6 +10,51 @@ import torch
 import os
 import pandas as pd
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+
+def GDPPLoss(phiFake, phiReal, backward=True):
+    r"""
+    Implementation of the GDPP loss. Can be used with any kind of GAN
+    architecture.
+
+    Args:
+
+        phiFake (tensor) : last feature layer of the discriminator on real data
+        phiReal (tensor) : last feature layer of the discriminator on fake data
+        backward (bool)  : should we perform the backward operation ?
+
+    Returns:
+
+        Loss's value. The backward operation in performed within this operator
+    """
+    def compute_diversity(phi):
+        phi = F.normalize(phi, p=2, dim=1)
+        SB = torch.mm(phi, phi.t())
+        eigVals, eigVecs = torch.symeig(SB, eigenvectors=True)
+        return eigVals, eigVecs
+
+    def normalize_min_max(eigVals):
+        minV, maxV = torch.min(eigVals), torch.max(eigVals)
+        if abs(minV - maxV) < 1e-10:
+            return eigVals
+        return (eigVals - minV) / (maxV - minV)
+
+    fakeEigVals, fakeEigVecs = compute_diversity(phiFake)
+    realEigVals, realEigVecs = compute_diversity(phiReal)
+
+    # Scaling factor to make the two losses operating in comparable ranges.
+    magnitudeLoss = 0.0001 * F.mse_loss(target=realEigVals, input=fakeEigVals)
+    structureLoss = -torch.sum(torch.mul(fakeEigVecs, realEigVecs), 0)
+    normalizedRealEigVals = normalize_min_max(realEigVals)
+    weightedStructureLoss = torch.sum(
+        torch.mul(normalizedRealEigVals, structureLoss))
+    gdppLoss = magnitudeLoss + weightedStructureLoss
+
+    if backward:
+        gdppLoss.backward(retain_graph=True)
+
+    return gdppLoss.item()
 
 
 class FullyConnectedGANExperimentBuilder(ExperimentBuilderGAN):
@@ -58,7 +103,7 @@ class FullyConnectedGANExperimentBuilder(ExperimentBuilderGAN):
         return fake_slates
 
     def update_discriminator(self, real_slates, user_interactions_with_padding, number_of_interactions_per_user):
-        self.discriminator.zero_grad()
+        self.optimizer_dis.zero_grad()
 
         dis_real = self.discriminator(real_slates, user_interactions_with_padding, number_of_interactions_per_user)
         dis_real = dis_real.mean()
@@ -100,10 +145,14 @@ class FullyConnectedGANExperimentBuilder(ExperimentBuilderGAN):
         return d_loss
 
     def update_generator(self, user_interactions_with_padding, number_of_interactions_per_user):
+        self.optimizer_gen.zero_grad()
+        self.optimizer_dis.zero_grad()
+
         noise = torch.randn(user_interactions_with_padding.shape[0], self.configs['noise_hidden_dims'],
                             dtype=torch.float32, device=self.device)
 
         slates = self.generator(user_interactions_with_padding, number_of_interactions_per_user, noise)
+
         loss = self.discriminator(slates, user_interactions_with_padding, number_of_interactions_per_user)
         loss = loss.mean()
         loss.backward()
