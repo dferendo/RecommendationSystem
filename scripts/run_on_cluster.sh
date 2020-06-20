9a5cf7a60f08fd266198908ea89b408e58287784
Dylan <dylanfrendo@gmail.com>
2020-06-20 02:04:05 +0200

    Added more experiments

--- scripts/run_on_cluster.sh
+++ scripts/run_on_cluster.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Teach-LongJobs
+#SBATCH --partition=Teach-Standard
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=2-08:00:00
+#SBATCH --time=0-08:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
